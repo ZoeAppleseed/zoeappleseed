a386c45acfe8c358fa303da2eae8d5c377939fe7
James Forbes <james.a.forbes@gmail.com>
2014-03-16 11:04:24 +1100

    Fixes #2

--- js/tumblr_load.js
+++ js/tumblr_load.js
@@ -35,7 +35,7 @@ function log_me(data){
 }
 
 function addToGallery(carouselLinks){
-
+    shuffle(carouselLinks);
     if(window.gallery){
         gallery.add(carouselLinks);
     } else {
@@ -44,6 +44,25 @@ function addToGallery(carouselLinks){
             carousel: true
         });
     }
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 
