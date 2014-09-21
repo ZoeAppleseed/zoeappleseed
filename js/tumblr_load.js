3c7337cbabb079e4d7469440f17adf9708f3e546
James <james.a.forbes@gmail.com>
2014-09-21 20:58:50 +1000

    Multiple request hack because tumblr api not working

--- js/tumblr_load.js
+++ js/tumblr_load.js
@@ -10,21 +10,50 @@
  */
 
 /* global, blueimp, $, tumblr_api_read */
-
+var i = 0;
 function loaded(data){
-   populate(data.response)
+   window.posts = window.posts || []
+   var seeds = _.filter(data.response.posts,function(post){
+      return _.contains(post.tags,'seed') 
+    })
+   window.posts.push.apply(window.posts,seeds)
+   i++;
+   if(i>6){ populate() }
+   
+
 }
 
 $(function () {
     'use strict';
      
     jQuery.ajax({
-        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?tag=seed&offset=0&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
+        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?offset=0&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
         dataType: 'jsonp'
     });
     
     jQuery.ajax({
-    	url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/info/?api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=bio',
-    	dataType: 'jsonp'
-    })
+        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?offset=20&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
+        dataType: 'jsonp'
+    });
+
+    jQuery.ajax({
+        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?offset=40&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
+        dataType: 'jsonp'
+    });
+    jQuery.ajax({
+        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?offset=60&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
+        dataType: 'jsonp'
+    });
+    jQuery.ajax({
+        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?offset=80&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
+        dataType: 'jsonp'
+    });
+    jQuery.ajax({
+        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?offset=100&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
+        dataType: 'jsonp'
+    });
+    jQuery.ajax({
+        url: 'http://api.tumblr.com/v2/blog/zoeappleseed.tumblr.com/posts/photo?offset=120&api_key=msIByDvkVk3gSr360nq2vmTkKIAvW4gNTB2dUYkvIO9NLwyxNy&jsonp=loaded', 
+        dataType: 'jsonp'
+    });
 });
